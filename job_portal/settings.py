7fd01bb82aa86150b5a3ae215a38776cdeab16c9
Peniel Negasa <penaye779@gmail.com>
2026-06-09 11:37:34 +0300

    Fix Cloudinary config

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -193,9 +193,4 @@ CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 # Used for media files since Render has no
 # persistent disk on free/starter plans.
 # ─────────────────────────────────────────
-CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
-    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
-    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
-}
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
